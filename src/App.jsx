5b85505962c06e994373bea6fa3b63d4300437e1
Jeremy Whitney <bearwhitness@gmail.com>
2024-07-17 11:24:32 -0500

    authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { AllPosts } from "./components/posts/AllPosts";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./components/auth/Login.jsx";
+import { Register } from "./components/auth/Register.jsx";
+import { ApplicationViews } from "./views/ApplicationViews";
+import { Authorized } from "./views/Authorized.jsx";
 
 export const App = () => {
   return (
-    <>
-      <AllPosts />
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="*"
+        element={
+          <Authorized>
+            <ApplicationViews />
+          </Authorized>
+        }
+      />
+    </Routes>
   );
 };
---
+++ src/views/ApplicationViews.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import { AllPosts } from "../components/posts/AllPosts";
+
+export const ApplicationViews = () => {
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index element={<AllPosts />} />
+      </Route>
+    </Routes>
+  );
+};
