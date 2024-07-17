56cbd31790e95677b3281650396d2a26c8621b5d
Jeremy Whitney <bearwhitness@gmail.com>
2024-07-17 13:01:52 -0500

    created navbar and currentUser

---
+++ src/components/nav/NavBar.jsx
@@ -0,0 +1,29 @@
+import { Link, useNavigate } from "react-router-dom";
+import "./NavBar.css";
+
+export const NavBar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <ul className="navbar">
+      <li className="navbar-item">
+        <Link to="/allPosts">All Posts</Link>
+      </li>
+      {localStorage.getItem("learning_user") ? (
+        <li className="navbar-item navbar-logout">
+          <Link
+            to="navbar-link"
+            onClick={() => {
+              localStorage.removeItem("learning_user");
+              navigate("/login", { replace: true });
+            }}
+          >
+            Logout
+          </Link>
+        </li>
+      ) : (
+        ""
+      )}
+    </ul>
+  );
+};
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,11 +1,29 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import { AllPosts } from "../components/posts/AllPosts";
+import { NavBar } from "../components/nav/NavBar";
+import { useEffect, useState } from "react";
 
 export const ApplicationViews = () => {
+    const [currentUser, setCurrentUser] = useState({})
+
+    useEffect(() => {
+        const localLearningUser = localStorage.getItem("learning_user")
+        const learningUserObject = JSON.parse(localLearningUser)
+        setCurrentUser(learningUserObject)
+    }, [])
+
   return (
     <Routes>
-      <Route path="/">
-        <Route index element={<AllPosts />} />
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route index element={<AllPosts currentUser={currentUser}/>} />
       </Route>
     </Routes>
   );
